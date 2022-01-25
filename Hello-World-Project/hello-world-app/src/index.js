c5ae3fe6d8f87097638e57d4318eec375a53a9f3
cgaona1 <gaonachris930@gmail.com>
2022-01-24 18:24:08 -0700

    Added a function and experimented more with expressions.

--- Hello-World-Project/hello-world-app/src/index.js
+++ Hello-World-Project/hello-world-app/src/index.js
@@ -4,11 +4,38 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
+//Embedding expressions
+const name = "Christopher";
+const element1 = <h1>Hello there, {name}!</h1>
+
+const user = {
+  firstName: 'Christopher',
+  lastName: 'Gaona'
+};
+
+//Creating a function
+function formatName(user){
+  return user.firstName + ' ' + user.lastName;
+};
+
+const element2 = (
+  <div>
+    <h1>Hello World!</h1>
+    <h2>
+      You're full name is: {formatName(user)}
+    </h2>
+  </div>
+);
+
 ReactDOM.render(
   //<React.StrictMode>
     //<App />
   //</React.StrictMode>,
-  <h1>Hello, world</h1>,
+
+  //element1,
+  element2,
   document.getElementById('root')
 );
 
