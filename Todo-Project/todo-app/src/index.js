38c03eb89adc9484c6440fd5c5ed1ba6001801d3
cgaona1 <gaonachris930@gmail.com>
2022-02-07 13:14:19 -0700

    Added a title

--- Todo-Project/todo-app/src/index.js
+++ Todo-Project/todo-app/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App subject='Flor'/>
   </React.StrictMode>,
   document.getElementById('root')
 );
