2e74393832c45912675f02a0d8c1e7b813a89770
cgaona1 <gaonachris930@gmail.com>
2022-02-07 18:08:14 -0700

    Added a text field with an add button

--- Todo-Project/todo-app/src/App.js
+++ Todo-Project/todo-app/src/App.js
@@ -4,7 +4,21 @@ function App(props) {
   return (
     <div className="todoapp">
       <h1>Todo App</h1>
-      
+      <form>
+        <h2>
+          What needs to be done?
+        </h2>
+        <input
+          type = "text"
+          id = "new-todo-input"
+          className = ""
+          name = "text"
+          autoComplete = "off"
+        />
+        <button type="submit" className="">
+          Add
+        </button>
+      </form>
     </div>
   );
 }
