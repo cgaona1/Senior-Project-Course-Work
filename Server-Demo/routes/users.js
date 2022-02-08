fbdf1b8e465f90311e4def1987d78b86ca0fcb73
cgaona1 <gaonachris930@gmail.com>
2022-02-08 16:45:30 -0700

    Add a get route for user; test

--- Server-Demo/routes/users.js
+++ Server-Demo/routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var users = [
+  {"id":1, "name":"O Moo"},
+  {"id":2, "name":"Chris"}
+]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/:id', function (req, res, next) {
+  var id = req.params.id;
+  console.log(id);
+
+  for(let user of users){
+    if(user.id == id){
+      res.json(user);
+    }
+  }
+  res.send('Cannot find user');
 });
 
 module.exports = router;
