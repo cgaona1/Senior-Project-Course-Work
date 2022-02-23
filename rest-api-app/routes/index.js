fc73e83935ef3e5ad976006e50bad8274507fc8a
cgaona1 <gaonachris930@gmail.com>
2022-02-22 21:16:28 -0700

    Added a hello world route

--- rest-api-app/routes/index.js
+++ rest-api-app/routes/index.js
@@ -4,6 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  next();
+});
+
+router.get('/hello', function(req, res, next){
+  res.send('Hello World!');
 });
 
 module.exports = router;
