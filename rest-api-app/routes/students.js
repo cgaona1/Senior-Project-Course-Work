882ec72b9902e282ff9ad7cc71cd2fae5efa9d75
cgaona1 <gaonachris930@gmail.com>
2022-02-24 16:27:45 -0700

    Post route created

--- rest-api-app/routes/students.js
+++ rest-api-app/routes/students.js
@@ -2,8 +2,26 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/students', (req, res) => {
-    res.send('Hello from student route');
-});
+const students = [
+    {
+        firstName: "Chris",
+        lastName: "Gaona"
+    },
+    {
+        firstName: "Flor",
+        lastName: "Gonzalez"
+    }
+]
+
+router.route('/students')
+    .get(function (req, res) {
+        console.log(students);
+        res.send('Hello from student route');
+    })
+    .post(function (req, res) {
+        console.log('Route Reached');
+        res.send('Route Reached');
+    });
+
 
 export default router;
