74baf3327803d15934914a54777f4c9c4211d929
cgaona1 <gaonachris930@gmail.com>
2022-03-02 11:58:31 -0700

    Added connection to  mongodb

--- rest-api-app/index.js
+++ rest-api-app/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 
 import studentsRoutes from './routes/students.js';
 
 const app = express();
 const PORT = 3000;
+
+//Database connection
+const CONNECTION_URL = 'mongodb+srv://cgaona:cgaona123@cluster0.upe4k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`)))
+    .catch((error) => console.log(error.message));
 
 app.use(bodyParser.json());
 app.use('/students', studentsRoutes);
@@ -13,4 +20,4 @@ app.get('/', (req, res) => {
     res.send('Hello from Homepage.');
 });
 
-app.listen(PORT, () => console.log(`Server running on port: http://localhost${PORT}`));
+//app.listen(PORT, () => console.log(`Server running on port: http://localhost${PORT}`));
